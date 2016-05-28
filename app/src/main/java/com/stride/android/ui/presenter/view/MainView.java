3451bd21b528fdcc912146131509b23ced3b0f15
connor <connor@memrise.com>
2016-05-28 17:13:50 +0100

    Fix bug where clicking a tab wouldn't show it as selected

--- app/src/main/java/com/stride/android/ui/presenter/view/MainView.java
+++ app/src/main/java/com/stride/android/ui/presenter/view/MainView.java
@@ -6,7 +6,6 @@ import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
-import android.support.v7.app.AppCompatActivity;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -23,6 +22,10 @@ import com.stride.android.ui.adapter.MainScreenPagerAdapter;
  */
 @AutoFactory public class MainView {
 
+  private static final int[] TABS = {
+      R.drawable.selector_dashboard, R.drawable.selector_trophy
+  };
+
   @BindView(R.id.main_pager) ViewPager mainPager;
 
   private final ActivityFacade mActivityFacade;
@@ -34,68 +37,60 @@ import com.stride.android.ui.adapter.MainScreenPagerAdapter;
 
     mTabLayout = (TabLayout) LayoutInflater.from(mActivityFacade.asActivity())
         .inflate(R.layout.toolbar_tabs, (ViewGroup) parent, false);
-    setupActionbar(parent, mActivityFacade.asActivity());
+    setupActionbar(parent, mActivityFacade);
     setAdapter(new MainScreenPagerAdapter(mActivityFacade.getFragmentManager()));
   }
 
-  private void setupActionbar(View view, AppCompatActivity activity) {
-    activity.getSupportActionBar().setDisplayShowCustomEnabled(true);
-    activity.getSupportActionBar().setDisplayShowHomeEnabled(false);
-    activity.getSupportActionBar().setDisplayShowTitleEnabled(false);
+  protected void setupActionbar(View view, ActivityFacade activity) {
+    activity.getActionbar().setDisplayShowCustomEnabled(true);
+    activity.getActionbar().setDisplayShowHomeEnabled(false);
+    activity.getActionbar().setDisplayShowTitleEnabled(false);
     final ActionBar.LayoutParams params =
         new ActionBar.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT,
             ViewGroup.LayoutParams.MATCH_PARENT);
     params.setMargins(view.getResources().getDimensionPixelSize(R.dimen.generic_margin_medium), 0,
         0, 0);
     params.gravity = Gravity.FILL_VERTICAL;
-    activity.getSupportActionBar().setCustomView(mTabLayout, params);
+    activity.getActionbar().setCustomView(mTabLayout, params);
   }
 
-  private void setAdapter(PagerAdapter adapter) {
-    mainPager.setAdapter(adapter);
-    mainPager.setOffscreenPageLimit(3);
+  public void setAdapter(final PagerAdapter adapter) {
     if (ViewCompat.isLaidOut(mTabLayout)) {
-      setupTabs();
+      setupTabs(adapter);
     } else {
       mTabLayout.addOnLayoutChangeListener(new View.OnLayoutChangeListener() {
         @Override
         public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft,
             int oldTop, int oldRight, int oldBottom) {
-          setupTabs();
+          setupTabs(adapter);
           mTabLayout.removeOnLayoutChangeListener(this);
         }
       });
     }
   }
 
-  private void setupTabs() {
-    mTabLayout.removeAllTabs();
-    mTabLayout.addTab(mTabLayout.newTab().setIcon(R.drawable.selector_dashboard));
-    mTabLayout.addTab(mTabLayout.newTab().setIcon(R.drawable.selector_trophy));
-    mainPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(mTabLayout) {
-      int mScrollState;
-      int mScrollPosition;
-      float mScrollOffset;
+  protected void setupTabs(PagerAdapter adapter) {
+    mainPager.setAdapter(adapter);
+    int count = adapter.getCount();
+    for (int i = 0; i < count; i++) {
+      final TabLayout.Tab tab = mTabLayout.newTab();
+      mTabLayout.addTab(tab, false);
+      tab.setCustomView(R.layout.layout_tab_view).setIcon(TABS[i]);
+    }
 
-      @Override public void onPageScrollStateChanged(int state) {
-        super.onPageScrollStateChanged(state);
-        mScrollState = state;
-      }
-
-      @Override
-      public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-        super.onPageScrolled(position, positionOffset, positionOffsetPixels);
-        mScrollPosition = position;
-        mScrollOffset = positionOffset;
-      }
-
-      @Override public void onPageSelected(int position) {
-        super.onPageSelected(position);
-        if (mScrollState != ViewPager.SCROLL_STATE_IDLE) {
-          mTabLayout.setScrollPosition(mScrollPosition, mScrollOffset, true);
-        }
-      }
-    });
+    final TabLayout.TabLayoutOnPageChangeListener tabLayoutPageListener =
+        new TabLayout.TabLayoutOnPageChangeListener(mTabLayout);
+    mainPager.addOnPageChangeListener(tabLayoutPageListener);
     mTabLayout.setOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mainPager));
+    if (mainPager.getCurrentItem() > 0) {
+      mainPager.post(new Runnable() {
+        @Override public void run() {
+          tabLayoutPageListener.onPageSelected(mainPager.getCurrentItem());
+          mTabLayout.getTabAt(mainPager.getCurrentItem()).select();
+        }
+      });
+    } else {
+      mTabLayout.getTabAt(mainPager.getCurrentItem()).select();
+    }
   }
 }
