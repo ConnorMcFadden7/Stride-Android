2d84e4971841b174d21b8ee0386a1facc25edce5
connor <connor@memrise.com>
2016-05-28 14:59:54 +0100

    Added some more achievements

--- app/src/main/java/com/stride/android/util/AchievementGenerator.java
+++ app/src/main/java/com/stride/android/util/AchievementGenerator.java
@@ -20,14 +20,42 @@ public class AchievementGenerator {
         R.drawable.ic_achievement_walk1_complete_big,
         R.drawable.ic_achievement_walk1_incomplete_big),
 
+    WALK_TWO(R.string.achievements_walk2_title, R.string.achievements_walk2_description,
+        R.drawable.ic_achievement_walk2_complete, R.drawable.ic_achievement_walk2_incomplete,
+        R.drawable.ic_achievement_walk2_complete_big,
+        R.drawable.ic_achievement_walk2_incomplete_big),
+
     BURN_ONE(R.string.achievements_burn1_title, R.string.achievements_burn1_description,
         R.drawable.ic_achievement_burn1_complete, R.drawable.ic_achievement_burn1_incomplete,
         R.drawable.ic_achievement_burn1_complete_big,
         R.drawable.ic_achievement_burn1_incomplete_big),
 
+    BURN_TWO(R.string.achievements_burn2_title, R.string.achievements_burn2_description,
+        R.drawable.ic_achievement_burn2_complete, R.drawable.ic_achievement_burn2_incomplete,
+        R.drawable.ic_achievement_burn2_complete_big,
+        R.drawable.ic_achievement_burn2_incomplete_big),
+
     REMOVE_ADS(R.string.achievements_ads_title, R.string.achievements_ads_description,
         R.drawable.ic_achievement_ads_complete, R.drawable.ic_achievement_ads_incomplete,
-        R.drawable.ic_achievement_ads_complete_big, R.drawable.ic_achievement_ads_incomplete_big);
+        R.drawable.ic_achievement_ads_complete_big, R.drawable.ic_achievement_ads_incomplete_big),
+
+    SET_GOAL(R.string.achievements_set_goal_title, R.string.achievements_set_goal_description,
+        R.drawable.ic_achievement_set_goal_complete, R.drawable.ic_achievement_set_goal_incomplete,
+        R.drawable.ic_achievement_set_goal_complete_big,
+        R.drawable.ic_achievement_set_goal_incomplete_big),
+
+    REACH_GOAL(R.string.achievements_reach_goal_title, R.string.achievements_reach_goal_description,
+        R.drawable.ic_achievement_reach_goal_complete,
+        R.drawable.ic_achievement_reach_goal_incomplete,
+        R.drawable.ic_achievement_reach_goal_complete_big,
+        R.drawable.ic_achievement_reach_goal_incomplete_big),
+
+    CUSTOM_GOAL(R.string.achievements_custom_goal_title,
+        R.string.achievements_custom_goal_description,
+        R.drawable.ic_achievement_custom_goal_complete,
+        R.drawable.ic_achievement_custom_goal_incomplete,
+        R.drawable.ic_achievement_custom_goal_complete_big,
+        R.drawable.ic_achievement_custom_goal_incomplete_big);
 
     private final @StringRes int title;
     private final @StringRes int description;
