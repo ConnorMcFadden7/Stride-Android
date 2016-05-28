13b89270d97cf32d6c89e67fcecd4d3a4fac6129
connor <connor@memrise.com>
2016-05-28 16:58:11 +0100

    Integrated AdMob - still needs the correct unit-ids once admob account is setup

--- app/src/main/java/com/stride/android/ui/activity/MainActivity.java
+++ app/src/main/java/com/stride/android/ui/activity/MainActivity.java
@@ -2,12 +2,18 @@ package com.stride.android.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import butterknife.BindView;
+import com.google.android.gms.ads.AdListener;
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+import com.google.android.gms.ads.InterstitialAd;
 import com.stride.android.R;
+import com.stride.android.data.local.PreferencesHelper;
 import com.stride.android.data.persistence.DatabaseHelper;
 import com.stride.android.ioc.ActivityComponent;
 import com.stride.android.service.SensorListener;
@@ -19,7 +25,11 @@ import javax.inject.Inject;
 
 public class MainActivity extends BaseActivity {
 
+  private static final String TEST_DEVICE_NEXUS_5 = "5E06724163EDE1D63AA54BFC8581E828";
+  private static final int DELAY_BEFORE_DISPLAYING_INT_ADS = 120000;
+
   @BindView(R.id.container_main) View mParent;
+  @BindView(R.id.ad_view) AdView mAdView;
 
   // maybe a better helper method like a Provider to get the steps
   @Inject DatabaseHelper databaseHelper;
@@ -27,7 +37,11 @@ public class MainActivity extends BaseActivity {
   @Inject MainViewFactory mainViewFactory;
   @Inject ActivityFacade activityFacade;
   @Inject PaymentSystem paymentSystem;
+  @Inject PreferencesHelper preferencesHelper;
 
+  private InterstitialAd mInterstitialAd;
+  private final Handler mAdHandler = new Handler();
+  private boolean mIsInterstitialAdShowing = false;
 
   @Override public boolean isHomeAsUpEnabled() {
     return false;
@@ -40,9 +54,24 @@ public class MainActivity extends BaseActivity {
     setSupportActionBar(toolbar);
     startService(new Intent(this, SensorListener.class));
     presentMainScreen();
+
+    //// TODO: 28/05/16
+    initAdMob();
+    //initInterstitialAds();
+
     paymentSystem.checkPurchaseStatus(new PaymentSystem.PurchaseHistory() {
       @Override public void onResponse() {
-        //hide ads
+        if (!preferencesHelper.hasUpgraded()) {
+
+          // initAdMob();
+
+          mAdHandler.postDelayed(new Runnable() {
+            public void run() {
+              //  initInterstitialAds();
+              mAdHandler.postDelayed(this, DELAY_BEFORE_DISPLAYING_INT_ADS);
+            }
+          }, DELAY_BEFORE_DISPLAYING_INT_ADS);
+        }
       }
     });
   }
@@ -73,5 +102,32 @@ public class MainActivity extends BaseActivity {
 
   private void presentMainScreen() {
     mainPresenter.present(mainViewFactory.create(mParent, activityFacade.wrap(this)));
+  }
+
+  private void initAdMob() {
+    AdRequest adRequest = new AdRequest.Builder().addTestDevice(TEST_DEVICE_NEXUS_5).build();
+
+    mAdView.setVisibility(View.VISIBLE);
+    mAdView.loadAd(adRequest);
+  }
+
+  private void initInterstitialAds() {
+    mInterstitialAd = new InterstitialAd(this);
+    mInterstitialAd.setAdUnitId(getResources().getString(R.string.interstitial_ad_unit_id));
+    AdRequest adRequest = new AdRequest.Builder().addTestDevice(TEST_DEVICE_NEXUS_5).build();
+    mInterstitialAd.loadAd(adRequest);
+    mInterstitialAd.setAdListener(new AdListener() {
+      @Override public void onAdLoaded() {
+        super.onAdLoaded();
+        if (!mIsInterstitialAdShowing) {
+          mIsInterstitialAdShowing = true;
+          mInterstitialAd.show();
+        }
+      }
+
+      @Override public void onAdClosed() {
+        mIsInterstitialAdShowing = false;
+      }
+    });
   }
 }
